d0b6d9ec60fd711874055c5820e885e7afa6670f
Ярослав <93127105+Yaroslavlazarenko@users.noreply.github.com>
2022-03-28 11:01:16 +0300

    optimized task 1 and 2

--- src/Main.java
+++ src/Main.java
@@ -10,25 +10,26 @@ public class Main {
         task5();
     }
 
-    /** 31
-     Даны вещественное число а и натуральное число n. Вычислить:
-     S=1/a + 1/a^2 + 1/a^4+...+1/a^(2n-2). */
+    /**
+     * 31
+     * Даны вещественное число а и натуральное число n. Вычислить:
+     * S=1/a + 1/a^2 + 1/a^4+...+1/a^(2n-2).
+     */
 
     private static void task1() {
         Scanner Scanner = new Scanner(System.in);
-        float real_number,sum=0;
-        int natural_number;
-        System.out.print("Enter real a: ");
-        real_number = Scanner.nextInt();
+        float sum = 0;
+        int naturalNumber;
         System.out.print("Enter natural n: ");
-        natural_number = Scanner.nextInt();
-        if(natural_number>0)
-        {
-            for (int i = 1; i <= natural_number; i++)
-                sum += 1 / Math.pow(real_number, (2 * natural_number - 2));
+        naturalNumber = Scanner.nextInt();
+        int denominator = naturalNumber;
+        if (naturalNumber > 0) {
+            for (int i = 1; i <= naturalNumber; i++) {
+                sum += 1 / denominator;
+                denominator *= denominator;
+            }
             System.out.print("Sum= " + sum);
-        }
-        else
+        } else
             System.out.print("Enter natural n.");
     }
 
@@ -37,23 +38,24 @@ public class Main {
      * Вычислить сумму: 1 + 1/3 + 1/3^2 + ... + 1/3^8.
      */
     private static void task2() {
-        double sum=0;
-        for(int degree=0;degree<=8;degree++)
-            sum=sum+1/Math.pow(3,degree);
-        System.out.println("Sum= "+sum);
+        double sum = 0;
+        double denominator = 1;
+        for (int degree = 0; degree <= 8; degree++) {
+            sum = sum + 1 / denominator;
+            denominator *= 3;
+        }
+        System.out.println("Sum= " + sum);
     }
 
     /**
      * 81
      * Даны числа а1, a2, …, а10. Определить их сумму.
      */
-    private static void task3()
-    {
-        int Array[] = {3,2,5,6,4,5,3,2,1,5};
-        int sum=0;
-        for(int number=0;number<=9;number++)
-            sum+=Array[number];
-        System.out.println("Sum = "+ sum);
+    private static void task3() {
+        int sum = 0;
+        for (int number = 0; number <= 9; number++)
+            sum +=(int)(Math.random()*200-100);
+        System.out.println("Sum = " + sum);
     }
 
     /**
@@ -66,12 +68,12 @@ public class Main {
         System.out.println("Enter the length of the array:");
         int lenght = Scanner.nextInt();
         int[] Array = new int[lenght];
-        int max=-1;
-        Array[lenght-1]=0;
-        for(int number=0;number<lenght-2;number++) {
-            Array[number] = (int) (Math.random()*(200+1)) - 100;
-            if (Array[number]>max)
-                max=Array[number];
+        int max = -1;
+        Array[lenght - 1] = 0;
+        for (int number = 0; number < lenght - 2; number++) {
+            Array[number] = (int) (Math.random() * (200 + 1)) - 100;
+            if (Array[number] > max)
+                max = Array[number];
         }
         System.out.println("Max number: " + max);
     }
@@ -82,10 +84,9 @@ public class Main {
      */
     private static void task5() {
         int sum;
-        for(int number=1000;number<10000;number++)
-        {
-            sum=number/1000+(number/100)%10+(number/10)%10+number%10;
-            if(Math.pow(sum,4)==number)
+        for (int number = 1000; number < 10000; number++) {
+            sum = number / 1000 + (number / 100) % 10 + (number / 10) % 10 + number % 10;
+            if (Math.pow(sum, 4) == number)
                 System.out.println("This number: " + number);
         }
 
